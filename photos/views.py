3a5277a1309046daeff0ccf09646833e6fe8a909
lethanhtupk <lethanhtu1551998@gmail.com>
2020-04-19 12:16:03 +0700

    update

--- photos/views.py
+++ photos/views.py
@@ -7,16 +7,16 @@ from rest_framework.decorators import api_view
 # Create your views here.
 
 
-def home(request):
-    i = random.randint(0, 1001)
-    photos = Photo.objects.all()[i:i+20]
-    return render(request, 'index.html', {'photos': photos})
+# def home(request):
+#     i = random.randint(0, 1001)
+#     photos = Photo.objects.all()[i:i+20]
+#     return render(request, 'index.html', {'photos': photos})
 
 
-def photos(request):
-    photos_list = Photo.objects.all()
-    paginator = Paginator(photos_list, 30)
-    page = request.GET.get('page') # get the page number
-    photos = paginator.get_page(page)
+# def photos(request):
+#     photos_list = Photo.objects.all()
+#     paginator = Paginator(photos_list, 30)
+#     page = request.GET.get('page') # get the page number
+#     photos = paginator.get_page(page)
 
-    return render(request, 'photos.html', {'photos': photos})
+#     return render(request, 'photos.html', {'photos': photos})
