910f177c378130f84bc63889f0dccd63b005595c
lethanhtupk <lethanhtu1551998@gmail.com>
2020-04-19 13:12:53 +0700

    updated

--- photos_gallery/settings.py
+++ photos_gallery/settings.py
@@ -141,7 +141,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # which origins will be accepted in django application
 CORS_ORIGIN_WHITELIST = (
