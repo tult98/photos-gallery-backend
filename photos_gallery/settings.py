662137d11693d7a56efefec138cdff874e396639
lethanhtupk <lethanhtu1551998@gmail.com>
2020-06-03 17:45:11 +0700

    updated

--- photos_gallery/settings.py
+++ photos_gallery/settings.py
@@ -158,8 +158,8 @@ STATICFILES_DIRS = [
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:8080',
     'http://127.0.0.1:8080',
-    # 'https://asiangirls.herokuapp.com/',
-    # 'http://asiangirls.herokuapp.com/',
+    'https://asiangirls.herokuapp.com',
+    'http://asiangirls.herokuapp.com',
 )
 
 
